283032aec26f6a6cf04d1515fde4a7d338ccfb49
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-04-08 13:50:50 -0300

    Iniciando o projeto

---
+++ Projeto/src/main/java/poo/Icalendar.java
@@ -0,0 +1,30 @@
+package poo;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.time.LocalDate;
+
+
+public class Icalendar {
+
+    public final static double VERSION = 2.0;
+    public static String PRODID = "PRODID";
+    public final static String PRODIFPADRAO = "Renan Rodolfo//Gerador de calendários iCalendar//PT_BR";
+
+    private static HashMap<String, ArrayList<String>> vcalendar = new HashMap<String, ArrayList<String>>();
+    private static ArrayList<String> eventos = new ArrayList();
+
+    public void alterarEvento(){
+
+    }
+
+    public void excluirEvento(){
+
+    }
+
+    public void addEvento(){
+
+    }
+
+
+}
